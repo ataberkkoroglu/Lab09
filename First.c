9e5a3620f0f5ec33735115b8722c1c8c7b25478c
Ataberk <ataberkkoroglu02@gmail.com>
2023-05-16 11:51:24 +0300

    Updated First Question's Answer

--- First.c
+++ First.c
@@ -9,9 +9,9 @@ int main(void){
     struct tm *Tm=localtime(&Time);
     printf("\e[1mWelcome To Program...\n%.2d : %.2d : %.2d  %.2d / %.2d / %.2d",Tm->tm_hour,Tm->tm_min,Tm->tm_sec,Tm->tm_mday,Tm->tm_mon+1,Tm->tm_year+1900);
 
-    char Text[20],Essay[1000],ch;
+    char Text[20],Essay[1000]="\0",ch;
     int point;
-    strcpy(Essay,"");
+    
     while(1){
     printf("\n\e[1;35mWrite a Character:\e[0m ");gets(Text);
     if (strcmp(Text,".")==0)
