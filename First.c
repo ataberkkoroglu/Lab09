7e8b4a23488bdb1f3e70801c6c80c61e7f09d2a3
Ataberk <ataberkkoroglu02@gmail.com>
2023-05-16 11:47:18 +0300

    Updated First Question's Answer

--- First.c
+++ First.c
@@ -1,29 +1,40 @@
 #include<stdio.h>
 #include<string.h>
-
+#include<time.h>
+#include<Windows.h>
 
 int main(void){
+    time_t Time;
+    Time=time(NULL);
+    struct tm *Tm=localtime(&Time);
+    printf("\e[1mWelcome To Program...\n%.2d : %.2d : %.2d  %.2d / %.2d / %.2d",Tm->tm_hour,Tm->tm_min,Tm->tm_sec,Tm->tm_mday,Tm->tm_mon+1,Tm->tm_year+1900);
 
-    char Text[20],prev_Text[20],Essay[1000];
+    char Text[20],Essay[1000],ch;
     int point;
-    for(int i=0;i<20;i++){
-        printf("Write a Character:");gets(Text);
-        for(int j=0;j<20;j++)
-          if (Text[j]=='\0')
-             break;
-          else if (Text[j]=='.')
-            point++;
-        if (strcmp(prev_Text,Text)!=0)
-           strcat(Essay,Text);  
-        if (point!=0)
-            break;
+    strcpy(Essay,"");
+    while(1){
+    printf("\n\e[1;35mWrite a Character:\e[0m ");gets(Text);
+    if (strcmp(Text,".")==0)
+        break;
+    
+    else if ((strlen(Text)+strlen(Essay))<1000){
         
-        strcpy(prev_Text,Text);
-        strcpy(Text,"");
+        strcat(Essay," ");          
+        strcat(Essay,Text);
+    }
+    if ((strlen(Text)+strlen(Essay))>1000){
+        break;
+    }
+         
+    strcpy(Text,"");  
+           
+        
+        
+        
           
     }
-   
- printf("Text: %s",Essay);
-    
- }
+ printf("Text:");puts(Essay);  
+ return 0;
+    }
+ 
 
